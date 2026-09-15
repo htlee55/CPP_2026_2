c891bbeee5d7bddd19dfbc65e188e60767db33f6
htlee55 <htlee55@naver.com>
2026-09-15 10:55:02 +0900

    Update ex_while.cpp

--- chapter02/ex_while.cpp
+++ chapter02/ex_while.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-    int i{0};          // 1. 초기 상태
+    int n{10};                  // 1. 초기 상태
 
-    while (i < 5) {    // 2. 반복 조건
-        cout << i << '\n';
-        ++i;           // 3. 상태 변화
+    while (n < 0) {            // 2. 반복 조건
+        cout << i << ' ';
+        --i;                   // 3. 상태 변화
     }
-
+    cout << "발사!\n");
     return 0;
 }
