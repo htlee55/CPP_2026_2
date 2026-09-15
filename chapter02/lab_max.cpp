ff4a413507a29fc74f29b1fa7a84f46e390cc75c
htlee55 <htlee55@naver.com>
2026-09-15 12:29:27 +0900

    Update lab_max.cpp

--- chapter02/lab_max.cpp
+++ chapter02/lab_max.cpp
@@ -1,15 +1,33 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
+int main()
+{
+    int list[10]{};
 
-int list[10]{};
+    // 난수 초기화
+    srand(static_cast<unsigned int>(time(nullptr)));
 
-for (int& elem : list) {
-    elem = rand() % 100 + 1;
-    cout << elem << " ";
+    // 배열에 1~100 사이의 난수 저장
+    for (int& elem : list) {
+		elem = rand() % 100 + 1;        // 1~100 사이의 난수 생성
+        cout << elem << " ";
+    }
+
+    cout << endl;
+
+    // 첫 번째 값을 최대값의 초기값으로 설정
+    int max{ list[0] };
+
+    // 배열을 순회하면서 최대값 찾기
+    for (int elem : list) {
+        if (elem > max)
+            max = elem;
+    }
+
+    cout << "최대값 = " << max << endl;
+
+    return 0;
 }
-
-int max{list[0]};
-for (int elem : list) {
-    if (elem > max)
-        max = elem;
-}
-cout << "최대값=" << max;
